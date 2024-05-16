e9dc65a24dc9627267e98d6a9588cc5db7dafd5f
nathanredblur <jon.nathan.rich@gmail.com>
2024-05-16 00:33:23 -0500

    Config tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from '@nextui-org/theme'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,5 +11,19 @@ module.exports = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui({
+    themes: {
+      light: {
+        colors: {
+          primary: "#BB33FF",
+        },
+      },
+      dark: {
+        colors: {
+          primary: "#BB33FF",
+        },
+        // ... rest of the colors
+      },
+    }
+  })],
 }
